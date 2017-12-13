b90370474cab39b8be26f7cc1c89460f127aad56
Grzegorz <g.mrukwa@gmail.com>
2017-12-13 10:34:09 +0100

    Create AssemblyStationFactory

---
+++ dotNET/Abstraction/AssemblyStationFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnifiedAutomation.UaBase;
+using UnifiedAutomation.UaServer;
+
+namespace PolslMacrocourse.DcsLab.Abstraction
+{
+    using AssemblyStation = ObjectNode;
+    using Directory = ObjectNode;
+
+    internal class AssemblyStationFactory
+    {
+
+        public AssemblyStationFactory(DcsLabNodeManager nodeManager)
+        {
+            _nodeManager = nodeManager;
+        }
+
+        public AssemblyStation Create(string name, Directory directory)
+        {
+            var settings = new CreateObjectSettings()
+            {
+                ParentNodeId = directory.NodeId,
+                ReferenceTypeId = UnifiedAutomation.UaBase.ReferenceTypeIds.Organizes,
+                RequestedNodeId = new NodeId(name, _nodeManager.InstanceNamespaceIndex),
+                BrowseName = new QualifiedName(name, _nodeManager.InstanceNamespaceIndex),
+                TypeDefinitionId = new NodeId(PolslMacrocourse.DcsLab.ObjectTypes.AssemblyStationType, _nodeManager.TypeNamespaceIndex)
+            };
+            var node = _nodeManager.CreateObject(_nodeManager.Server.DefaultRequestContext, settings);
+            return node;
+        }
+
+        private readonly DcsLabNodeManager _nodeManager;
+    }
+}
