0e99afcaac8d1ec6a0b4a0132d85f2cebe877492
Grzegorz <g.mrukwa@gmail.com>
2017-12-13 20:32:36 +0100

    Add ServerContext on Client side

---
+++ dotNET/AssemblyStationClient/StationAutomation/ServerContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using UnifiedAutomation.UaBase;
+using UnifiedAutomation.UaClient;
+
+namespace AssemblyStationClient.StationAutomation
+{
+    internal class ServerContext : IDisposable
+    {
+        private bool _disposed;
+
+        public ServerContext(ApplicationInstance applicationInstance, string serverUrl)
+        {
+            Session = new Session(applicationInstance);
+            Session.ConnectionStatusUpdate += ThrowOnConnectionLost;
+            Session.UseDnsNameAndPortFromDiscoveryUrl = true;
+            Session.Connect(serverUrl, SecuritySelection.None);
+        }
+
+        public Session Session { get; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~ServerContext()
+        {
+            Dispose(false);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                Session.Disconnect();
+                Session.Dispose();
+                _disposed = true;
+            }
+        }
+
+        private void ThrowOnConnectionLost(Session sender, ServerConnectionStatusUpdateEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case ServerConnectionStatus.Disconnected:
+                case ServerConnectionStatus.LicenseExpired:
+                case ServerConnectionStatus.ServerShutdown:
+                case ServerConnectionStatus.ServerShutdownInProgress:
+                    throw new IOException("Connection to the server lost.");
+                case ServerConnectionStatus.Connected:
+                case ServerConnectionStatus.ConnectionWarningWatchdogTimeout:
+                case ServerConnectionStatus.ConnectionErrorClientReconnect:
+                case ServerConnectionStatus.SessionAutomaticallyRecreated:
+                case ServerConnectionStatus.Connecting:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
