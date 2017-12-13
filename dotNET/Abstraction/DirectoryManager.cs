bb91b714194dbe8ebb15d3604e125e8d0c867247
Grzegorz <g.mrukwa@gmail.com>
2017-12-13 10:33:48 +0100

    Create DirectoryManager abstraction

---
+++ dotNET/Abstraction/DirectoryManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnifiedAutomation.UaBase;
+using UnifiedAutomation.UaServer;
+
+namespace PolslMacrocourse.DcsLab.Abstraction
+{
+    using Directory = ObjectNode;
+
+    internal class DirectoryManager
+    {
+        public DirectoryManager(DcsLabNodeManager nodeManager)
+        {
+            _nodeManager = nodeManager;
+        }
+
+        public Directory Get(ExpandedNodeId expandedNodeId)
+        {
+            var nodeId = new NodeId((uint)expandedNodeId.Identifier, _nodeManager.InstanceNamespaceIndex);
+            var node = _nodeManager.FindInMemoryNode(nodeId);
+            return (Directory)node;
+        }
+
+        private readonly DcsLabNodeManager _nodeManager;
+        private RequestContext Context => _nodeManager.Server.DefaultRequestContext;
+    }
+}
