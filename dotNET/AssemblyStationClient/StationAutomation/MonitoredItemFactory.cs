f3a558b4ce413343941b499456c1bc0f40ca8d3e
Grzegorz <g.mrukwa@gmail.com>
2017-12-17 23:54:14 +0100

    Add MonitoredItemFactory

---
+++ dotNET/AssemblyStationClient/StationAutomation/MonitoredItemFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnifiedAutomation.UaBase;
+using UnifiedAutomation.UaClient;
+
+namespace AssemblyStationClient.StationAutomation
+{
+    class MonitoredItemFactory
+    {
+        public MonitoredItemFactory(Session session, string instanceName, string namespaceName)
+        {
+            _instanceName = instanceName;
+            var info = new SessionInfo(session);
+            _namespaceIndex = info.GetNamespaceIndex(namespaceName);
+        }
+
+        public MonitoredItem Create<T>(string fieldName, Action<T> setter)
+        {
+            var nodeId = new NodeId($"{_instanceName}.{fieldName}", _namespaceIndex);
+            var normalizedSetter = new Action<object>(o => setter((T)o));
+            return new DataMonitoredItem(nodeId)
+            {
+                UserData = normalizedSetter
+            };
+        }
+
+        private readonly string _instanceName;
+        private readonly ushort _namespaceIndex;
+    }
+}
