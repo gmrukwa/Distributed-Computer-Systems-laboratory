053c7962edf68b1e47e0bcbb5b5106a6351e6d7e
Grzegorz <g.mrukwa@gmail.com>
2017-12-18 08:41:23 +0100

    Start UA ApplicationInstance in the App constructor

--- dotNET/AssemblyStationClient/App.xaml.cs
+++ dotNET/AssemblyStationClient/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using UnifiedAutomation.UaBase;
 
 namespace AssemblyStationClient
 {
@@ -13,5 +14,16 @@ namespace AssemblyStationClient
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // applications without a UnifiedAutomation license embedded as a resource will stop working after 1 hour.
+            ApplicationLicenseManager.AddProcessLicenses(System.Reflection.Assembly.GetExecutingAssembly(), "UnifiedAutomation.Sample.License.License.lic");
+
+            // Create the certificate if it does not exist yet
+            ApplicationInstance.Default.AutoCreateCertificate = true;
+
+            // start the application.
+            ApplicationInstance.Default.Start();
+        }
     }
 }
